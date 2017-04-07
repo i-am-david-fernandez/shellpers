8280342f9443c4da32b98563b397b533ede9600e
David Fernandez <i.am.david.fernandez@gmail.com>
2017-04-07 16:35:34 +1000

    <DF> Added symbolic colour definitions, refactored coloured echos to use.

--- coloured_echo.sh
+++ coloured_echo.sh
@@ -5,35 +5,50 @@
 ## on black background).
 
 ## The names suggest their intended use:
-##   - InfoMajor produces bright green text for important information,
-##   - InfoMinor produces comparatively dull blue text for information of secondary importance,
-##   - Error produces bright red text for critically-important information.
+##   - info_major produces bright green text for (good) important information,
+##   - info_minor produces comparatively dull blue text for (good) information of secondary importance,
+##   - warning produces comparatively dull yellow text for (bad) information of secondary importance,
+##   - error produces bright red text for (bad) critically-important information.
 
 ## Usage:
-##  echoXYZ "This is a coloured message!"
+##  echo_xyz "This is a coloured message!"
 ## e.g.,
 ##    echo_info_major "Performing first stage processing of large dataset..."
 ##    echo_info_minor "  - Doing the first thing..."
 ##    echo_info_minor "  - Reticulating splines..."
 ##    echo_info_major "Complete."
+##    echo_warning "Warning! Probability of failure to reticulate increasing."
 ##    echo_error "Error! Splines not fully reticulated; system unstable, terminal failure imminent!"
+
+source colour_definitions.sh
 
 echo_info_major()
 {
-    echo -e "\033[32m$*"
-    echo -en "\033[0m"
+    echo -e ${ANSI_ESCAPE}${ANSI_COLOUR_GREEN}$*${ANSI_RESET}
 }
 
 echo_info_minor()
 {
-    echo -e "\033[94m$*"
-    echo -en "\033[0m"
+    echo -e ${ANSI_ESCAPE}${ANSI_COLOUR_LIGHT_BLUE}$*${ANSI_RESET}
+}
+
+echo_warning()
+{
+    echo -e ${ANSI_ESCAPE}${ANSI_COLOUR_YELLOW}$*${ANSI_RESET}
 }
 
 echo_error()
 {
-    echo -e "\033[31m$*"
-    echo -en "\033[0m"
+    echo -e ${ANSI_ESCAPE}${ANSI_COLOUR_RED}$*${ANSI_RESET}
 }
+
+demo_echos()
+{
+    echo_info_major "Major thing"
+    echo_info_minor "Minor thing"
+    echo_warning "Warning thing"
+    echo_error "Error thing"
+}
+
 ## │ </DF>                                                                      │
 ## └────────────────────────────────────────────────────────────────────────────┘
