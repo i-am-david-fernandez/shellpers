5259be122e2915809a3f689518d9217de66e0362
David Fernandez <i.am.david.fernandez@gmail.com>
2017-04-28 10:03:47 +1000

    <DF> Added query option to up to allow simply querying/printing of resultant directory without change.

--- up.sh
+++ up.sh
@@ -2,13 +2,27 @@
 ## │ <DF>                                                                       │
 ## A helper function to allow quick and convenient traversal up through the directory tree.
 ## Use 'up' to navigate to parent (first ancestor) directory, 'up N' to navigate to N'th ancestor.
+## Using the option '-q' will simply query and print the resultant directory without
+## changing to it.
 up ()
 {
     height=1;
-    if [ $1 ]
-    then
-        height=$1;
-    fi
+
+    for arg in $*
+    do
+        case "$arg" in
+            "-q")
+                shift
+                d=`up $*; pwd`
+                echo $d
+                return
+            ;;
+            *)
+                height=$arg
+                ;;
+        esac
+        shift
+    done
 
     for i in `seq $height`
     do
